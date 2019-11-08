9b0b12b2e2d3659f6a0fd3050089830ab8bebd49
ANA  LIDIA DA SILVA DANTAS <24102@ETECPAOF.LAN>
2019-11-08 20:18:50 -0300

    update jean

---
+++ Exerícios Manzano/página 89/ExercícioA/ExercícioA/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercícioA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = new double[12];
+            int i, j;
+            double x;
+
+            for (i=0; i<12; i++)
+            {
+                Console.Write("Digite o {0}º valor de A: ", i + 1);
+                a[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (i=0; i<12; i++)
+            {
+                for (j=i+1; j<12; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        x = a[i];
+                        a[i] = a[j];
+                        a[j] = x;
+                    }
+                }
+            }
+            
+            for (i=0; i<12; i++)
+            {
+                Console.WriteLine("O {0}º valor de A é {1}.", i + 1, a[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
